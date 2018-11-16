c056151867e35e8f4ed4faddc007b484148ca356
AlexanderSergan <jfdi686@gmail.com>
2018-11-16 14:35:36 +0200

    Demo bot;

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.warn('hola world i am linted on precommit hook xD')
+import TelegramBot from 'node-telegram-bot-api'
+
+const token = process.env.TG_TOKEN || 'no_token'
+
+console.warn(`
+  hola world i am linted on precommit hook xD
+  telegram token: ${token}
+`,
+)
+
+const bot = new TelegramBot(token, { polling: true})
+
+interface INote {
+  uid: string
+  text: string
+  time: string
+}
+
+const notes: INote[] = []
+
+bot.onText(/\/Нагадай (.+) в (.+)/, (msg, match) => {
+  if (!!msg && !!match) {
+
+    const userId = msg.from.id
+    const text = match[1]
+    const time = match[2]
+
+    notes.push({ uid: userId, time, text })
+    bot.sendMessage(userId, 'Супра! обовʼязково нагадаю при попутному вітрі :)')
+  } else if (!match[1]) {
+    bot.sendMessage(msg.from.id, 'Не зрозумів')
+  } else if (!match[2]) {
+    bot.sendMessage(msg.from.id, 'Не зрозумів коли')
+  }
+
+})
+
+setInterval( () => {
+  for (let i = 0; i < notes.length; i++) {
+    const curDate = new Date().getHours() + ':' + new Date().getMinutes()
+    if (notes[i].time === curDate) {
+      bot.sendMessage(notes[i].uid, 'Братан, не забудь: ' + notes[i].text )
+      notes.splice(i, 1)
+    }
+  }
+}, 1000)
